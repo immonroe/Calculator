f2f6e32c0e7c02501577a431f51f09e9c341364a
immonroe <immanuelmonroe@gmail.com>
2022-10-20 08:17:35 -0400

    Added math functions to js

---
+++ script.js
@@ -0,0 +1,19 @@
+const add = function(x,y) {
+    return x + y;
+  };
+  
+const subtract = function(x,y) {
+      return x - y;
+  };
+
+  const multiply = function(array) {
+    var sum=1;
+      for (var i=0; i<array.length; i++) {
+          sum = sum * array[i];
+      } 
+      return sum;
+  };
+
+const divide = function() {
+    
+};
