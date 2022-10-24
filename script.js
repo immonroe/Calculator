1123b71a68af94a71ee6de7811f331dbda97e4e4
immonroe <immanuelmonroe@gmail.com>
2022-10-24 18:03:12 -0400

    Added operate() function and tested in console

--- script.js
+++ script.js
@@ -15,3 +15,16 @@ const divide = (x,y) => {
 };
 
 // Create a new function operate that takes an operator and 2 numbers and then calls one of the above functions on the numbers.
+
+function calculate(x, y, operate) {
+    var operators = {
+      '+': function (a, b) { return a + b },
+      '-': function (a, b) { return a - b },
+      '*': function (a, b) { return a * b },
+      '/': function (a, b) { return a / b },
+      '%': function (a, b) { return a % b },
+      '**': function (a, b) { return a ** b }
+    }
+  
+    return operate in operators ? operators[operate](x, y) : NaN
+  }
