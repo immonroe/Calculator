2a6937734d262dce417a6bf797175d388442b9c4
immonroe <immanuelmonroe@gmail.com>
2022-11-01 20:16:07 -0400

    Added variables to store content in js

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+// DOM
+
+const numberButton = document.querySelectorAll('.number');
+const operatorButton = document.querySelectorAll('.operator');
+const clearButton = document.querySelector('.clear');
+const deleteButton = document.querySelector('.delete');
+const showResult = document.querySelector('.result');
+const currentOperand = document.querySelector('.current-operand');
+const previousOperand = document.querySelector('.previous-operand');
+const equalsKey = document.querySelector('.equal-sign');
+
+currentOperand.textContent = ' ';
+previousOperand.textContent = ' ';
+
 // basic operators for calculator
 
 const add = (x,y) => {
@@ -18,7 +32,7 @@ const divide = (x,y) => {
 
 // Create a new function operate that takes an operator and 2 numbers and then calls one of the above functions on the numbers.
 
-function calculate(x, y, operate) {
+function calculate(x, operate, y) {
     var operators = {
       '+': function (a, b) { return a + b },
       '-': function (a, b) { return a - b },
@@ -31,4 +45,10 @@ function calculate(x, y, operate) {
 
 // Create the functions that populate the display when you click the number buttons.
 // You should be storing the ‘display value’ in a variable somewhere for use in the next step.
+
+let storedNumber = '';
+let clickedOperator = ''
+let firstNumber = '';
+let result = '';
+currentOperand.textContent = 0;
 
